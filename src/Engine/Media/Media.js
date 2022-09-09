f8c1c25411b325a6a6ab622bfc1227286699fe42
Matteo Leclercq <matteo.ftt@gmail.com>
2022-09-09 16:16:01 +0200

    ✨ Adding circular UI effect

--- src/Engine/Media/Media.js
+++ src/Engine/Media/Media.js
@@ -1,30 +1,31 @@
-import {LinearFilter, ShaderMaterial, Mesh} from "three";
+import {LinearFilter, Mesh, ShaderMaterial} from "three";
 import vertexShader from '/static/shaders/image-vertex.glsl';
 import fragmentShader from '/static/shaders/image-fragment.glsl';
 import Engine from "../Engine";
+import {map} from "../Utils/Utils.js";
 
 export default class {
-    constructor ({ geometry, gl, image, index, length, renderer, scene, screen, text, viewport }) {
+    constructor({geometry, gl, image, index, length, renderer, scene, screen, text, viewport}) {
         this.experience = new Engine();
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
 
-        this.geometry = geometry
-        this.gl = gl
-        this.image = image
-        this.index = index
-        this.length = length
-        this.scene = scene
-        this.screen = screen
-        this.text = text
-        this.viewport = viewport
+        this.geometry = geometry;
+        this.gl = gl;
+        this.image = image;
+        this.index = index;
+        this.length = length;
+        this.scene = scene;
+        this.screen = screen;
+        this.text = text;
+        this.viewport = viewport;
 
         this.extra = 0;
 
-        this.createShader()
-        this.createMesh()
+        this.createShader();
+        this.createMesh();
 
-        this.onResize()
+        this.onResize();
     }
 
     createShader() {
@@ -37,70 +38,73 @@ export default class {
             vertexShader,
             fragmentShader,
             uniforms: {
-                tMap: { value: texture },
-                uPlaneSizes: { value: [0, 0] },
-                uImageSizes: { value: [0, 0] },
-                uViewportSizes: { value: [this.viewport.width, this.viewport.height] }
+                tMap: {value: texture},
+                uPlaneSizes: {value: [0, 0]},
+                uImageSizes: {value: [0, 0]},
+                uViewportSizes: {value: [this.viewport.width, this.viewport.height]}
             },
             transparent: true,
-        })
+        });
 
-        this.shader.uniforms.uImageSizes.value = [texture.source.data.naturalWidth, texture.source.data.naturalHeight]
+        this.shader.uniforms.uImageSizes.value = [texture.source.data.naturalWidth, texture.source.data.naturalHeight];
     }
 
     createMesh() {
-        this.plane = new Mesh(this.geometry, this.shader)
-        this.scene.add(this.plane)
+        this.plane = new Mesh(this.geometry, this.shader);
+        this.scene.add(this.plane);
     }
 
-    onResize ({ screen, viewport } = {}) {
+    onResize({screen, viewport} = {}) {
         if (screen) {
-            this.screen = screen
+            this.screen = screen;
         }
 
         if (viewport) {
-            this.viewport = viewport
+            this.viewport = viewport;
 
-            this.shader.uniforms.uViewportSizes.value = [this.viewport.width, this.viewport.height]
+            this.shader.uniforms.uViewportSizes.value = [this.viewport.width, this.viewport.height];
         }
 
-        this.scale = this.screen.height / 1500
+        this.scale = this.screen.height / 1500;
 
-        this.plane.scale.y = this.viewport.height * (900 * this.scale) / this.screen.height
-        this.plane.scale.x = this.viewport.width * (700 * this.scale) / this.screen.width
+        this.plane.scale.y = this.viewport.height * (900 * this.scale) / this.screen.height;
+        this.plane.scale.x = this.viewport.width * (700 * this.scale) / this.screen.width;
 
-        this.shader.uniforms.uPlaneSizes.value = [this.plane.scale.x, this.plane.scale.y]
+        this.shader.uniforms.uPlaneSizes.value = [this.plane.scale.x, this.plane.scale.y];
 
         // position all the rectangles in the x axis, making sure we have a small gap between them
-        this.padding = 2
+        this.padding = 2;
 
-        this.width = this.plane.scale.x + this.padding
-        this.widthTotal = this.width * this.length
+        this.width = this.plane.scale.x + this.padding;
+        this.widthTotal = this.width * this.length;
 
-        this.x = this.width * this.index
+        this.x = this.width * this.index;
     }
 
-    update (scroll, direction) {
-        this.plane.position.x = this.x - scroll.current * 0.1 - this.extra
+    update(scroll, direction) {
+        this.plane.rotation.z = map(this.plane.position.x, -this.widthTotal, this.widthTotal, Math.PI, -Math.PI)
 
-        const planeOffset = this.plane.scale.x / 2
-        const viewportOffset = this.viewport.width
+        this.plane.position.x = this.x - scroll.current * 0.1 - this.extra;
+        this.plane.position.y = Math.cos((this.plane.position.x / this.widthTotal) * Math.PI) * 75 - 75
 
-        this.isBefore = this.plane.position.x + planeOffset < -viewportOffset
-        this.isAfter = this.plane.position.x - planeOffset > viewportOffset
+        const planeOffset = this.plane.scale.x / 2;
+        const viewportOffset = this.viewport.width;
+
+        this.isBefore = this.plane.position.x + planeOffset < -viewportOffset;
+        this.isAfter = this.plane.position.x - planeOffset > viewportOffset;
 
         if (direction === 'right' && this.isBefore) {
-            this.extra -= this.widthTotal
+            this.extra -= this.widthTotal;
 
-            this.isBefore = false
-            this.isAfter = false
+            this.isBefore = false;
+            this.isAfter = false;
         }
 
         if (direction === 'left' && this.isAfter) {
-            this.extra += this.widthTotal
+            this.extra += this.widthTotal;
 
-            this.isBefore = false
-            this.isAfter = false
+            this.isBefore = false;
+            this.isAfter = false;
         }
     }
 }
